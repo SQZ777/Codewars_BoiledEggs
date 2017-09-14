1ec7ab5e818fc62911525b50a924147478d9e86a
sqz777 <sqz777isme@gmail.com>
2017-09-14 22:21:55 +0800

    First FAILED

--- Codewars_BoiledEggs/UnitTest1.cs
+++ Codewars_BoiledEggs/UnitTest1.cs
@@ -6,6 +6,24 @@ namespace BoiledEggs
     [TestClass]
     public class UnitTest1
     {
-        
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //arrange
+            var input = 0;
+            var expected = 0;
+            //act
+            var actual = Kata.CookingTime(input);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public static int CookingTime(int input)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
